819ce22931ec96e358a4e86c2c8c70b93dade9ce
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 18:51:54 -0500

    Python Variable Annotations, Basics Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Basic Annotations
+concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Return a concatenated
+        string
+    """
+    return str1 + str2
