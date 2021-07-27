9461a43b1eeb37e6b14f3b3b9d6496caf89fb3a4
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 18:30:56 -0500

    Python Variable Annotations, Basics

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Basic Annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Function to add and
+    return float type variable
+    """
+    return a + b
