a10fc436df81ad6dbb57849ea6dd1b27bf5a2e02
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 21:49:42 -0500

    Python Variable Annotations,check iterable object for typing

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from typing import Sequence, Tuple, Iterable, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return an iterable object
+
+    Args:
+        lst (Iterable[Sequence]): Iterable obj to check
+
+    Returns:
+        List[Tuple[Sequence, int]]: List of elements iterable
+    """
+    return [(i, len(i)) for i in lst]
