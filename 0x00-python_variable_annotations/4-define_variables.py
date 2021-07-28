b188a85c4c6206694af5c0dfa84dfdb43d3e892a
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 19:40:38 -0500

    Python Variable Annotations, Define variables, check notation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,7 +3,7 @@
 defined and typed on assignation
 """
 
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = str("Holberton")
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
