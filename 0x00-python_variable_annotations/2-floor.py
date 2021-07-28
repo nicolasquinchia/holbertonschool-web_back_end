a619d6b2407d2ccf1e2bdba42a646f3ab8f78bea
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 19:02:56 -0500

    Python Variable Annotations, Basics Floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic Annotations Floor
+"""
+
+
+def floor(n: float) -> int:
+    """Return a typed variable
+
+    Args:
+        n (float): Floor to describe
+
+    Returns:
+        int: Integer of the floor
+    """
+    return int(n)
