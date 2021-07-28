94fbcbb56ccddd411662c18559956eb968ce628d
nicolasquinchia <nicolasquinchia@gmail.com>
2021-07-27 19:09:22 -0500

    Python Variable Annotations, Basics to String

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Annotations to String
+"""
+
+
+def to_str(n: float) -> str:
+    """ Gives a representations of
+    a variable in other type
+
+    Args:
+        n (float): Variable to represent
+
+    Returns:
+        str: string representation
+    """
+    return str(n)
