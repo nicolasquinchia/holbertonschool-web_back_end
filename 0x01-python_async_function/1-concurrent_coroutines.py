bc3b095140bb602b53192517285528fc60d2eb1a
nicolasquinchia <nicolasquinchia@gmail.com>
2021-08-03 22:35:34 -0500

    Async, Concurrent coroutines, check pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Let's execute multiple coroutines
+"""Execute multiple coroutines
 at the same time with async
 """
 import asyncio
@@ -9,8 +9,18 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Function that append delays based on
+    the time that a imported function is called
+
+    Args:
+        n (int): Times to call the function
+        max_delay (int): upper limit of the randomized delay
+
+    Returns:
+        List[float]: List of delays on each call
+    """
     function_calls = []
-    for i in range (n):
+    for i in range(n):
         function_calls.append(asyncio.create_task(wait_random(max_delay)))
 
     delay_list = []
